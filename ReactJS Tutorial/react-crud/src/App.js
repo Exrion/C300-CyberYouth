ddc5cc47b59d6efc7f3e3ce3d45f5040cb4cfbac
Titus Lim <titus.happy@gmail.com>
2022-04-23 14:59:55 +0800

    ReactJS Tutorial webpage now functional Issue: Does not render components.

--- ReactJS Tutorial/react-crud/src/App.js
+++ ReactJS Tutorial/react-crud/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component, lazy } from "react";
 import { Route, Link, Routes, BrowserRouter } from "react-router-dom";
@@ -15,51 +14,39 @@ const Tutorial = lazy(() => import('./components/tutorial.component.js'));
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <BrowserRouter>
-          <nav className="navbar navbar-expand navbar-dark bg-dark">
-            <a href="/tutorials" className="navbar-brand">
-              bezKoder
-            </a>
-            <div className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link to={"/tutorials"} className="nav-link">
-                  Tutorials
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to={"/add"} className="nav-link">
-                  Add
-                </Link>
-              </li>
-            </div>
-          </nav>
-          <div className="container mt-3">
-            <Routes>
-              {/* <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
+      <>
+        <div className="App">
+          <BrowserRouter>
+            <nav className="navbar navbar-expand navbar-dark bg-dark">
+              <a href="/tutorials" className="navbar-brand">
+                bezKoder
+              </a>
+              <div className="navbar-nav mr-auto">
+                <li className="nav-item">
+                  <Link to={"/tutorials"} className="nav-link">
+                    Tutorials
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={"/add"} className="nav-link">
+                    Add
+                  </Link>
+                </li>
+              </div>
+            </nav>
+            <div className="container mt-3">
+              <Routes>
+                {/* <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
               <Route exact path="/add" component={AddTutorial} />
               <Route path="/tutorials/:id" component={Tutorial} /> */}
-              <Route path="/tutorials" Component={<TutorialsList />} />
-              <Route path="/add" Component={<AddTutorial />} />
-              <Route path="/tutorials/:id" Component={<Tutorial />} />
-            </Routes>
-          </div>
-        </BrowserRouter>
-      </div>
+                <Route path="/tutorials" Component={<TutorialsList />} />
+                <Route path="/add" Component={<AddTutorial />} />
+                <Route path="/tutorials/:id" Component={<Tutorial />} />
+              </Routes>
+            </div>
+          </BrowserRouter>
+        </div>
+      </>
     );
   }
 }
