07c06e9679c148500b1092f2cb1c752cd37a2eb8
Titus Lim <titus.happy@gmail.com>
2022-05-04 15:35:18 +0800

    Fix required for component dashboard

--- cyc-admin_panel/src/App.js
+++ cyc-admin_panel/src/App.js
@@ -56,32 +56,34 @@ function App() {
         </div>
       </nav>
 
-      {/* Configuration Items Flexbox */}
-      <div class="flex flex-wrap text-l bg-gray-100 rounded p-4 ">
-        <div class="p-4 text-black bg-gray-100 ">
+      <div>
+        {/* Configuration Items Flexbox */}
+        <div div class="flex flex-wrap text-l bg-gray-100 rounded p-4 " >
+          <div class="p-4 text-black bg-gray-100 ">
             <Link to={"/configAnnouncements"} className="nav-link">
               Announcements
             </Link>
-        </div>
-        <div class="p-4 text-black bg-gray-100">
+          </div>
+          <div class="p-4 text-black bg-gray-100">
             <Link to={"/configExchangeItems"} className="nav-link">
               Exchange Items
             </Link>
-        </div>
-        <div class="p-4 text-black bg-gray-100">
+          </div>
+          <div class="p-4 text-black bg-gray-100">
             <Link to={"/configTiers"} className="nav-link">
               Tiers
             </Link>
-        </div>
-        <div class="p-4 text-black bg-gray-100">
+          </div>
+          <div class="p-4 text-black bg-gray-100">
             <Link to={"/configTracks"} className="nav-link">
               Tracks
             </Link>
-        </div>
-        <div class="p-4 text-black bg-gray-100">
+          </div>
+          <div class="p-4 text-black bg-gray-100">
             <Link to={"/configTrophies"} className="nav-link">
               Trophies
             </Link>
+          </div>
         </div>
       </div>
 
---
+++ cyc-admin_panel/src/components/dashboard.componenet.js
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+import React, { Component } from "react";
+
+export default class dashboard extends Component {
+    render() {
+        return (
+            <div>
+                {/* Configuration Items Flexbox */}
+                <div div class="flex flex-wrap text-l bg-gray-100 rounded p-4 " >
+                    <div class="p-4 text-black bg-gray-100 ">
+                        <Link to={"/configAnnouncements"} className="nav-link">
+                            Announcements
+                        </Link>
+                    </div>
+                    <div class="p-4 text-black bg-gray-100">
+                        <Link to={"/configExchangeItems"} className="nav-link">
+                            Exchange Items
+                        </Link>
+                    </div>
+                    <div class="p-4 text-black bg-gray-100">
+                        <Link to={"/configTiers"} className="nav-link">
+                            Tiers
+                        </Link>
+                    </div>
+                    <div class="p-4 text-black bg-gray-100">
+                        <Link to={"/configTracks"} className="nav-link">
+                            Tracks
+                        </Link>
+                    </div>
+                    <div class="p-4 text-black bg-gray-100">
+                        <Link to={"/configTrophies"} className="nav-link">
+                            Trophies
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
