ee5cd2f6f9d4c9185d953019844a8246b1705b65
Titus Lim <titus.happy@gmail.com>
2022-05-04 12:27:19 +0800

    Added component pages and nav bar

--- cyc-admin_panel/src/App.js
+++ cyc-admin_panel/src/App.js
@@ -3,9 +3,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      {/* Navigation Bar */}
+      <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+        <div class="container flex flex-wrap justify-between items-center mx-auto">
+          <a href="" class="flex items-center">
+            <img src="cyc-admin_panel\src\images\navbar\CYS-Favicon.png" class="mr-3 h-6 sm:h-9" alt="CYS Logo" />
+            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Cyber Youth Singapore</span>
+          </a>
+        </div>
+      </nav>
+
+      {/* Main Content */}
+      <div>
+
+      </div>
     </div>
   );
 }
