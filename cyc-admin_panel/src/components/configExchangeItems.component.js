5f84d95d7f1d267f4ddd1c69e68ddd93a13811e8
BjornRPSOI <20034186@myrp.edu.sg>
2022-05-07 23:15:22 +0800

    Added sample table for each view page

--- cyc-admin_panel/src/components/configExchangeItems.component.js
+++ cyc-admin_panel/src/components/configExchangeItems.component.js
@@ -1,13 +1,37 @@
 import React, { Component } from "react";
-
+import { JsonToTable } from "react-json-to-table";
 
 export default class ConfigExchangeitems extends Component{
     render() {
+        function HTTPGET_Tiers() {
+            var axios = require('axios');
+
+            var config = {
+                method: 'get',
+                url: 'https://api.nest.cyberyouth.sg/api/tier',
+                headers: {
+                    "Access-Control-Allow-Origin": "*"
+                }
+            };
+
+            axios(config)
+                .then(function (response) {
+                    return JSON.stringify(response.data);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                    return null;
+                });
+        }
+
+        const tiersJson = require('../temp/apis/exchanges.json');
+
         return (
             <div>
-                
-                <h1>Hello World</h1>
-                
+                <p>Display a breadcrumb here</p>
+                <h1>Tiers</h1>
+                <JsonToTable json={HTTPGET_Tiers()} />
+                <JsonToTable json={tiersJson}/>
             </div>
         );
     }
--- cyc-admin_panel/src/components/configTracks.component.js
+++ cyc-admin_panel/src/components/configTracks.component.js
@@ -1,13 +1,38 @@
 import React, { Component } from "react";
+import { JsonToTable } from "react-json-to-table";
 
 
 export default class ConfigTracks extends Component{
     render() {
+        function HTTPGET_Tiers() {
+            var axios = require('axios');
+
+            var config = {
+                method: 'get',
+                url: 'https://api.nest.cyberyouth.sg/api/tier',
+                headers: {
+                    "Access-Control-Allow-Origin": "*"
+                }
+            };
+
+            axios(config)
+                .then(function (response) {
+                    return JSON.stringify(response.data);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                    return null;
+                });
+        }
+
+        const tiersJson = require('../temp/apis/tracks.json');
+
         return (
             <div>
-                
-                <h1>Hello World</h1>
-                
+                <p>Display a breadcrumb here</p>
+                <h1>Tiers</h1>
+                <JsonToTable json={HTTPGET_Tiers()} />
+                <JsonToTable json={tiersJson}/>
             </div>
         );
     }
