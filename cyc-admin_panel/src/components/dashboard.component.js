dfff3160be3de895ab88ff4f5bad404e26574c3b
Titus Lim <titus.happy@gmail.com>
2022-05-07 22:00:52 +0800

    fixed relative path for dashboard images

--- cyc-admin_panel/src/components/dashboard.component.js
+++ cyc-admin_panel/src/components/dashboard.component.js
@@ -11,31 +11,31 @@ export default class Dashboard extends Component {
                 <div div class="flex flex-wrap text-l bg-gray-100 rounded p-4 " >
                     <div class="p-4 text-black bg-gray-100 ">
                         <Link to={"./configAnnouncements"} className="nav-link">
-                        <img src={require('./images/dashboard/Announcement.png')} alt= "Announcement" class="h-50 w-50"></img>
+                        <img src={require('../images/dashboard/Announcement.png')} alt= "Announcement" class="h-50 w-50"></img>
                             Announcements
                         </Link>
                     </div>
                     <div class="p-4 text-black bg-gray-100">
                         <Link to={"/configExchangeItems"} className="nav-link">
-                        <img src={require('./images/dashboard/Gift.png')} alt= "Exchange Item" class="h-50 w-50 "></img>
+                        <img src={require('../images/dashboard/Gift.png')} alt= "Exchange Item" class="h-50 w-50 "></img>
                             Exchange Items
                         </Link>
                     </div>
                     <div class="p-4 text-black bg-gray-100">
                         <Link to={"/configTiers"} className="nav-link">
-                        <img src={require('./images/dashboard/Tiers.png')} alt= "Tiers" class="h-50 w-50 content-center"></img>
+                        <img src={require('../images/dashboard/Tiers.png')} alt= "Tiers" class="h-50 w-50 content-center"></img>
                             Tiers
                         </Link>
                     </div>
                     <div class="p-4 text-black bg-gray-100">
                         <Link to={"/configTracks"} className="nav-link">
-                        <img src={require('./images/dashboard/Tracks.png')} alt= "Tracks" class="h-50 w-50"></img>
+                        <img src={require('../images/dashboard/Tracks.png')} alt= "Tracks" class="h-50 w-50"></img>
                             Tracks
                         </Link>
                     </div>
                     <div class="p-4 text-black bg-gray-100">
                         <Link to={"/configTrophies"} className="nav-link">
-                        <img src={require('./images/dashboard/Trophies.png')} alt= "Trophies" class="h-50 w-50 content-center"></img>
+                        <img src={require('../images/dashboard/Trophies.png')} alt= "Trophies" class="h-50 w-50 content-center"></img>
                             Trophies
                         </Link>
                     </div>
