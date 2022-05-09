f2cc4d888a550b6d88875e2aac43a038b6ab168e
BjornRPSOI <20034186@myrp.edu.sg>
2022-05-09 17:03:28 +0800

    Got a example form for add tiers page

--- cyc-admin_panel/src/components/configTiers.component.js
+++ cyc-admin_panel/src/components/configTiers.component.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Rou, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { JsonToTable } from "react-json-to-table";
 
 
--- cyc-admin_panel/src/components/configTracks.component.js
+++ cyc-admin_panel/src/components/configTracks.component.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { JsonToTable } from "react-json-to-table";
-
+import { Link } from "react-router-dom";
 
 export default class ConfigTracks extends Component{
     render() {
