a77f73b2ed81a3d28db7801f2c855586aa0b6c5a
20045524 <charisseyee.ite@gmail.com>
2022-05-11 10:23:18 +0800

    updates on add page

--- cyc-admin_panel/src/components/addAnnouncements.component.js
+++ cyc-admin_panel/src/components/addAnnouncements.component.js
@@ -7,11 +7,135 @@ export default class addTiers extends Component{
         return (
             
 
-            <div>
-                
-                <p>Hello addTiers</p>
-                
-            </div>
+            <div class="block p-6 rounded-lg shadow-lg bg-white max-w-md">
+            <form>
+
+            <div class="form-group mb-6">
+                <input type="text" class="form-control block
+                  w-full
+                  px-3
+                  py-1.5
+                  text-base
+                  font-normal
+                  text-gray-700
+                  bg-white bg-clip-padding
+                  border border-solid border-gray-300
+                  rounded
+                  transition
+                  ease-in-out
+                  m-0
+                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput11"
+                  placeholder="Announcement Id:" />
+              </div>
+
+              <div class="form-group mb-6">
+                <input type="text" class="form-control block
+                  w-full
+                  px-3
+                  py-1.5
+                  text-base
+                  font-normal
+                  text-gray-700
+                  bg-white bg-clip-padding
+                  border border-solid border-gray-300
+                  rounded
+                  transition
+                  ease-in-out
+                  m-0
+                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput7"
+                  placeholder="Announcement Title:" />
+              </div>
+              <div class="form-group mb-6">
+                <input type="text" class="form-control block
+                  w-full
+                  px-3
+                  py-1.5
+                  text-base
+                  font-normal
+                  text-gray-700
+                  bg-white bg-clip-padding
+                  border border-solid border-gray-300
+                  rounded
+                  transition
+                  ease-in-out
+                  m-0
+                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput8"
+                  placeholder="Announcement Type:" />
+              </div>
+              <div class="form-group mb-6">
+                <input type="text" class="form-control block
+                  w-full
+                  px-3
+                  py-1.5
+                  text-base
+                  font-normal
+                  text-gray-700
+                  bg-white bg-clip-padding
+                  border border-solid border-gray-300
+                  rounded
+                  transition
+                  ease-in-out
+                  m-0
+                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput9"
+                  placeholder="Announcement Body:" />
+              </div>
+
+              <div class="form-group mb-6">
+                <input type="text" class="form-control block
+                  w-full
+                  px-3
+                  py-1.5
+                  text-base
+                  font-normal
+                  text-gray-700
+                  bg-white bg-clip-padding
+                  border border-solid border-gray-300
+                  rounded
+                  transition
+                  ease-in-out
+                  m-0
+                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput10"
+                  placeholder="Announcement Img:" />
+              </div>
+
+              <div class="form-group mb-6">
+                <input type="text" class="form-control block
+                  w-full
+                  px-3
+                  py-1.5
+                  text-base
+                  font-normal
+                  text-gray-700
+                  bg-white bg-clip-padding
+                  border border-solid border-gray-300
+                  rounded
+                  transition
+                  ease-in-out
+                  m-0
+                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput11"
+                  placeholder="Announcement Link:" />
+              </div>
+
+              <button type="submit" class="
+                w-full
+                px-6
+                py-2.5
+                bg-gray-100
+                text-black
+                font-medium
+                text-xs
+                leading-tight
+                uppercase
+                rounded
+                shadow-md
+                hover:bg-gray-200 hover:shadow-lg
+                focus:bg-gray-200 focus:shadow-lg focus:outline-none focus:ring-0
+                active:bg-gray-100 active:shadow-lg
+                transition
+                duration-150
+                ease-in-out">Send</button>
+            </form>
+          </div>
         );
     }
 }
--- cyc-admin_panel/src/components/addTracks.component.js
+++ cyc-admin_panel/src/components/addTracks.component.js
@@ -6,12 +6,154 @@ export default class addTiers extends Component{
         
         return (
             
+            
+            <div class="block p-6 rounded-lg shadow-lg bg-white max-w-md">
+            <form>
 
-            <div>
-                
-                <p>Hello addTiers</p>
-                
-            </div>
+            <div class="form-group mb-6">
+                <input type="text" class="form-control block
+                  w-full
+                  px-3
+                  py-1.5
+                  text-base
+                  font-normal
+                  text-gray-700
+                  bg-white bg-clip-padding
+                  border border-solid border-gray-300
+                  rounded
+                  transition
+                  ease-in-out
+                  m-0
+                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput11"
+                  placeholder="Track Id:" />
+              </div>
+
+              <div class="form-group mb-6">
+                <input type="text" class="form-control block
+                  w-full
+                  px-3
+                  py-1.5
+                  text-base
+                  font-normal
+                  text-gray-700
+                  bg-white bg-clip-padding
+                  border border-solid border-gray-300
+                  rounded
+                  transition
+                  ease-in-out
+                  m-0
+                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput7"
+                  placeholder="Track Name:" />
+              </div>
+              <div class="form-group mb-6">
+                <input type="text" class="form-control block
+                  w-full
+                  px-3
+                  py-1.5
+                  text-base
+                  font-normal
+                  text-gray-700
+                  bg-white bg-clip-padding
+                  border border-solid border-gray-300
+                  rounded
+                  transition
+                  ease-in-out
+                  m-0
+                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput8"
+                  placeholder="Track Description:" />
+              </div>
+              <div class="form-group mb-6">
+                <input type="text" class="form-control block
+                  w-full
+                  px-3
+                  py-1.5
+                  text-base
+                  font-normal
+                  text-gray-700
+                  bg-white bg-clip-padding
+                  border border-solid border-gray-300
+                  rounded
+                  transition
+                  ease-in-out
+                  m-0
+                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput9"
+                  placeholder="Track Provider:" />
+              </div>
+
+              <div class="form-group mb-6">
+                <input type="text" class="form-control block
+                  w-full
+                  px-3
+                  py-1.5
+                  text-base
+                  font-normal
+                  text-gray-700
+                  bg-white bg-clip-padding
+                  border border-solid border-gray-300
+                  rounded
+                  transition
+                  ease-in-out
+                  m-0
+                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput10"
+                  placeholder="Track Link:" />
+              </div>
+
+              <div class="form-group mb-6">
+                <input type="text" class="form-control block
+                  w-full
+                  px-3
+                  py-1.5
+                  text-base
+                  font-normal
+                  text-gray-700
+                  bg-white bg-clip-padding
+                  border border-solid border-gray-300
+                  rounded
+                  transition
+                  ease-in-out
+                  m-0
+                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput11"
+                  placeholder="Track Tags:" />
+              </div>
+              
+              <div class="form-group mb-6">
+                <input type="text" class="form-control block
+                  w-full
+                  px-3
+                  py-1.5
+                  text-base
+                  font-normal
+                  text-gray-700
+                  bg-white bg-clip-padding
+                  border border-solid border-gray-300
+                  rounded
+                  transition
+                  ease-in-out
+                  m-0
+                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput11"
+                  placeholder="Track Lemons:" />
+              </div>
+
+              <button type="submit" class="
+                w-full
+                px-6
+                py-2.5
+                bg-gray-100
+                text-black
+                font-medium
+                text-xs
+                leading-tight
+                uppercase
+                rounded
+                shadow-md
+                hover:bg-gray-200 hover:shadow-lg
+                focus:bg-gray-200 focus:shadow-lg focus:outline-none focus:ring-0
+                active:bg-gray-100 active:shadow-lg
+                transition
+                duration-150
+                ease-in-out">Send</button>
+            </form>
+          </div>
         );
     }
 }
