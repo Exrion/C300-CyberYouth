d6e4317e4959ae0055a3841e287f27bef52767aa
Titus Lim <titus.happy@gmail.com>
2022-05-15 18:24:54 +0800

    fixed code conflicts

--- cyc-admin_panel/src/http-common.js
+++ cyc-admin_panel/src/http-common.js
@@ -1,15 +1,7 @@
 import axios from "axios";
 export default axios.create({
-<<<<<<< HEAD
-  baseURL: "http://localhost:3001/api",
-  headers: {
-    "Content-type": "application/json",
-  },
-});
-=======
   baseURL: "http://localhost:8080/api",
   headers: {
     "Content-type": "application/json"
   }
 });
->>>>>>> 8948b5d28c491f605d10b78cbcb545cc4edebd39
