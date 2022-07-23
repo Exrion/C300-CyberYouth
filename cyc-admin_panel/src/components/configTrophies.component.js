4b9b3366e2c056d019ecd49ecebd61a67d02f173
Titus Lim <titus.happy@gmail.com>
2022-07-23 15:30:29 +0800

    Fixed padding for edit buttons

--- cyc-admin_panel/src/components/configTrophies.component.js
+++ cyc-admin_panel/src/components/configTrophies.component.js
@@ -134,7 +134,7 @@ export default class ConfigTrophies extends Component {
                                                             <div class="bg-blue-600 h-2.5 rounded-full" style={{width: `${parseInt(item.totalProgress)}%`}}></div>
                                                         </div>
                                                     </div>
-                                                    <div class="row-span-1 flex justify-center">
+                                                    <div class="row-span-1 flex justify-center mt-2">
                                                         <Link to={"/trophies/" + item.id} class="focus:outline-none text-white bg-green-700 hover:bg-green-800 transition ease-in-out focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">
                                                             Edit
                                                         </Link>
