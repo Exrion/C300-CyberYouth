6d768ae4f8c7a49bcbc678547b98840279b857eb
BjornRPSOI <bjorntinkarg@yahoo.com.sg>
2022-07-24 22:10:36 +0800

    Works for me

--- cyc-node/app/controllers/email.controller.js
+++ cyc-node/app/controllers/email.controller.js
@@ -5,15 +5,12 @@ exports.create = (req, res) => {
     let transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-          user:process.env.GMAIL_USER,
-          pass:process.env.GMAIL_PASS,
-
-          // type: 'OAuth2',
-          // user: "cycfyp2022@gmail.com",
-          // pass: "^$Wha3KbMBkH4Aa*D%vbrxKA76Q55BKNQ@c$!#QS9w",
-          // clientId: "610493552508-f9i2qp90jufn1ajia1usvk50ugg6te5l.apps.googleusercontent.com",
-          // clientSecret: "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",
-          // refreshToken: "1//04X86t9bbZW5dCgYIARAAGAQSNwF-L9IrOyCNMOZpJ53opGNkc_fINT3SOg4Nu2SdrvEaxeqUsdbOoiQ-4pMf7TLRFGbkw40oHw0"
+          type: 'OAuth2',
+          user: "cycfyp2022@gmail.com",
+          pass: "^$Wha3KbMBkH4Aa*D%vbrxKA76Q55BKNQ@c$!#QS9w",
+          clientId: "610493552508-f9i2qp90jufn1ajia1usvk50ugg6te5l.apps.googleusercontent.com",
+          clientSecret: "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",
+          refreshToken: "1//04X86t9bbZW5dCgYIARAAGAQSNwF-L9IrOyCNMOZpJ53opGNkc_fINT3SOg4Nu2SdrvEaxeqUsdbOoiQ-4pMf7TLRFGbkw40oHw0"
         }
       });
     
@@ -21,8 +18,8 @@ exports.create = (req, res) => {
     let mailOptions = {
         from: "cycfyp2022@gmail.com",
         to: req.body.email,
-        subject: req.body.subject,
-        text: req.body.text,
+        subject:  req.body.subject,
+        text: req.body.text
       };
 
 
@@ -30,9 +27,6 @@ exports.create = (req, res) => {
       if (err) {
         console.log("Error " + err);
       } else {
-        res.send({
-          message: "Email sent successfully"
-        });
         console.log("Email sent successfully");
       }
     });
