6bc9382b0eacc2d82f2d2fdbb4f25a63240c3449
Titus Lim <titus.happy@gmail.com>
2022-07-24 14:55:45 +0800

    Email details delegated to .env file

--- cyc-node/app/controllers/email.controller.js
+++ cyc-node/app/controllers/email.controller.js
@@ -5,12 +5,15 @@ exports.create = (req, res) => {
     let transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-          type: 'OAuth2',
-          user: "cycfyp2022@gmail.com",
-          pass: "^$Wha3KbMBkH4Aa*D%vbrxKA76Q55BKNQ@c$!#QS9w",
-          clientId: "610493552508-f9i2qp90jufn1ajia1usvk50ugg6te5l.apps.googleusercontent.com",
-          clientSecret: "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",
-          refreshToken: "1//04X86t9bbZW5dCgYIARAAGAQSNwF-L9IrOyCNMOZpJ53opGNkc_fINT3SOg4Nu2SdrvEaxeqUsdbOoiQ-4pMf7TLRFGbkw40oHw0"
+          user:process.env.GMAIL_USER,
+          pass:process.env.GMAIL_PASS,
+
+          // type: 'OAuth2',
+          // user: "cycfyp2022@gmail.com",
+          // pass: "^$Wha3KbMBkH4Aa*D%vbrxKA76Q55BKNQ@c$!#QS9w",
+          // clientId: "610493552508-f9i2qp90jufn1ajia1usvk50ugg6te5l.apps.googleusercontent.com",
+          // clientSecret: "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",
+          // refreshToken: "1//04X86t9bbZW5dCgYIARAAGAQSNwF-L9IrOyCNMOZpJ53opGNkc_fINT3SOg4Nu2SdrvEaxeqUsdbOoiQ-4pMf7TLRFGbkw40oHw0"
         }
       });
     
@@ -27,6 +30,9 @@ exports.create = (req, res) => {
       if (err) {
         console.log("Error " + err);
       } else {
+        res.send({
+          message: "Email sent successfully"
+        });
         console.log("Email sent successfully");
       }
     });
