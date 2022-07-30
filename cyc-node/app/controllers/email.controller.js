6e7c69ba84c06f0c7870f75ca0265350b526b45f
BjornRPSOI <bjorntinkarg@yahoo.com.sg>
2022-07-30 12:57:02 +0800

    email.controller

--- cyc-node/app/controllers/email.controller.js
+++ cyc-node/app/controllers/email.controller.js
@@ -5,12 +5,8 @@ exports.create = (req, res) => {
     let transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-          type: 'OAuth2',
-          user: "cycfyp2022@gmail.com",
-          pass: "^$Wha3KbMBkH4Aa*D%vbrxKA76Q55BKNQ@c$!#QS9w",
-          clientId: "610493552508-f9i2qp90jufn1ajia1usvk50ugg6te5l.apps.googleusercontent.com",
-          clientSecret: "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",
-          refreshToken: "1//04X86t9bbZW5dCgYIARAAGAQSNwF-L9IrOyCNMOZpJ53opGNkc_fINT3SOg4Nu2SdrvEaxeqUsdbOoiQ-4pMf7TLRFGbkw40oHw0"
+          user:process.env.GMAIL_USER,
+          pass:process.env.GMAIL_PASS,
         }
       });
     
