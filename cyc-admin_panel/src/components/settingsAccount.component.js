973c689c8a6124639a8f165b652ea96e8e8dfee8
Titus Lim <titus.happy@gmail.com>
2022-07-31 11:37:06 +0800

    Fixed UI scaling issues with smaller screens

--- cyc-admin_panel/src/components/settingsAccount.component.js
+++ cyc-admin_panel/src/components/settingsAccount.component.js
@@ -128,7 +128,7 @@ const SettingAccount = () => {
       {/* Content */}
       <div class="flex flex-col space-y-5 p-10 xl:grid xl:grid-cols-5">
         {/* Profile */}
-        <div class="flex flex-row group relative drop-shadow-2xl rounded-full p-5 xl:col-span-2">
+        <div class="flex flex-row group relative drop-shadow-2xl rounded-full p-5 xl:col-span-3">
           {/* Picture */}
           <img src={require('../images/navbar/pepe.png')} className="rounded-full h-9 w-9 w-32 h-32 ease-in-out group-hover:brightness-95 hover:border-2" alt='user portrait' />
           <div class="ml-20 flex flex-col">
@@ -145,24 +145,24 @@ const SettingAccount = () => {
         </div>
 
         {/* LinkedIn Integration */}
-        <div class="flex flex-col space-y-5 xl:col-span-1">
+        <div class="flex flex-col space-y-5 xl:col-span-1 md:ml-5 sm:ml-5">
           <div class="flex flex-col place-items-start">
             <p class="text-2xl text-bold text-left">LinkedIn Account</p>
             <p class="text-md text-slate-700 text-left">Shortcuts to your LinkedIn</p>
           </div>
-          <button class="bg-blue-700 hover:bg-blue-800 text-white rounded-full py-1 w-4/5"
+          <button class="text-sm font-medium bg-blue-700 hover:bg-blue-800 text-white rounded-full py-1 xl:w-4/5 md:w-2/5 sm:w-2/5 xs:2/5"
             onClick={Post}>
             Add Post
           </button>
-          <button class="bg-blue-700 hover:bg-blue-800 text-white rounded-full py-1 w-4/5"
+          <button class="text-sm font-medium bg-blue-700 hover:bg-blue-800 text-white rounded-full py-1 xl:w-4/5 md:w-2/5 sm:w-2/5 xs:2/5"
             onClick={HashTag}>
             Add HashTag
           </button>
-          <button class="bg-blue-700 hover:bg-blue-800 text-white rounded-full py-1 w-4/5"
+          <button class="text-sm font-medium bg-blue-700 hover:bg-blue-800 text-white rounded-full py-1 xl:w-4/5 md:w-2/5 sm:w-2/5 xs:2/5"
             onClick={Event}>
             Add Event
           </button>
-          <button class="bg-blue-700 hover:bg-blue-800 text-white rounded-full py-1 w-4/5"
+          <button class="text-sm font-medium bg-blue-700 hover:bg-blue-800 text-white rounded-full py-1 xl:w-4/5 md:w-2/5 sm:w-2/5 xs:2/5"
             onClick={EditLinkedIn}>
             Edit LinkedIn Page
           </button>
@@ -171,21 +171,21 @@ const SettingAccount = () => {
         <div class="flex flex-col space-y-5 xl:col-span-1 ml-5">
           {/* Password */}
           <div class="flex flex-col place-items-start">
-            <p class="text-2xl text-bold">
+            <p class="text-2xl text-bold text-left">
               Password
             </p>
             <p class="text-md text-slate-700 text-left">Last changed: 17 September 2021</p>
-            <button class="flex flex-row justify-center place-items-center space-x-2 mt-2 h-7 ease-in-out text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none">
-              <p>Change Password</p>
+            <button class="flex flex-row justify-center place-items-center space-x-2 mt-2 h-7 ease-in-out text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-3 mr-2 mb-2 focus:outline-none xl:py-5">
+              Change Password
             </button>
           </div>
 
           {/* Logout */}
           <div class="flex flex-col place-items-start">
-            <p class="text-2xl text-bold">
+            <p class="text-2xl text-bold text-left">
               Logout
             </p>
-            <p class="text-md text-slate-700">Logout of account</p>
+            <p class="text-md text-slate-700 text-left">Logout of account</p>
             <button class="flex flex-row justify-center place-items-center space-x-2 mt-2 h-7 ease-in-out text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2"
               onClick={logout}
             >
@@ -194,10 +194,10 @@ const SettingAccount = () => {
           </div>
 
           <div class="flex flex-col place-items-start">
-            <p class="text-2xl text-bold">Connect with LinkedIn</p>
-            <p class="text-left text-md text-slate-700">Connect your NEST panel account with LinkedIn!</p>
+            <p class="text-2xl text-bold text-left">Connect with LinkedIn</p>
+            <p class="text-left text-md text-slate-700 text-left">Connect your NEST panel account with LinkedIn!</p>
             <button
-              class="flex flex-row justify-center place-items-center space-x-2 mt-2 h-7 ease-in-out text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none"
+              class="flex flex-row justify-center place-items-center space-x-2 mt-2 h-7 ease-in-out text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none xl:py-5"
               onClick={linkedInLogin}
             >
               <p>Connect With LinkedIn</p>
